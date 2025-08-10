57875fb6301e7f47c0a437539e9fea61dadebe93
Ayan10Das <ayandas62898@gmail.com>
2025-08-10 21:23:46 +0530

    header change

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -20,14 +20,14 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   const [openDialog, setOpenDialog] = useState(false);
-  const [imgError , setImgError]=useState(false);
+  const [imgError, setImgError] = useState(false);
 
-  const user =JSON.parse(localStorage.getItem('user')) || "";
+  const user = JSON.parse(localStorage.getItem('user')) || "";
   useEffect(() => {
     console.log(user);
   }, []);
 
-    const login = useGoogleLogin({
+  const login = useGoogleLogin({
     onSuccess: (response) => {
       getUserProfile(response)
       // setOpenDialog(false)
@@ -35,7 +35,7 @@ function Header() {
     onError: (error) => console.log(error)
   });
 
-    const getUserProfile = (tokenInfo) => {
+  const getUserProfile = (tokenInfo) => {
     axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`, {
       headers: {
         Authorization: `Bearer ${tokenInfo.access_token}`,
@@ -57,77 +57,80 @@ function Header() {
 
       <div>
         {user ?
-          <div className='flex gap-3 items-center'>
+          <div className='flex gap-3 justify-center items-center'>
 
 
             <div className='flex  gap-2 justify-center items-center'>
 
-            <Link to="/">
-            <Button className='rounded-2xl  transition-all delay-100 duration-150 place-self-end-safe hover:bg-orange-500 hover:text-black cursor-pointer hover:scale-x-105'> Home </Button>
-            </Link>
+              <Link to="/">
+                <Button className='rounded-2xl  transition-all delay-100 duration-150 place-self-end-safe hover:bg-orange-500 hover:text-black cursor-pointer hover:scale-x-105'> Home </Button>
+              </Link>
 
-            <Link to="my-trips">
-            <Button className='rounded-2xl transition-all delay-100 duration-150 place-self-end-safe hover:bg-orange-500 hover:text-black cursor-pointer hover:scale-x-105'> My trips</Button>
-            </Link>
-            
-            
-             
-            <Button className='rounded-2xl transition-all delay-100 duration-150 place-self-end-safe hover:bg-orange-500 hover:text-black cursor-pointer hover:scale-x-105'
-            onClick={()=>{
-              googleLogout()
-              localStorage.removeItem('user');
-              window.location.href="/";
-            }}
-            > Logout </Button>
-           
+              <Link to="my-trips">
+                <Button className='rounded-2xl transition-all delay-100 duration-150 place-self-end-safe hover:bg-orange-500 hover:text-black cursor-pointer hover:scale-x-105'> My trips</Button>
+              </Link>
 
-                       {user?.picture && !imgError ? (
-              <img
-                src={user?.picture}
-                className="h-10 w-11 rounded-full object-cover  hidden md:block"
-                alt="user"
-                onError={()=> setImgError(true) }
-              />
-              
-            ) :
-            <div >
-            <FaUserCircle className='h-10 w-12 text-gray-500  hidden md:block'/>
-            {/* <div className='p-1 font-light text-sm'>{user.name}</div> */}
-            </div> 
+              <div>
 
-            }
+                <Button className='rounded-2xl transition-all delay-100 duration-150 place-self-end-safe hover:bg-orange-500 hover:text-black cursor-pointer hover:scale-x-105'
+                  onClick={() => {
+                    googleLogout()
+                    localStorage.removeItem('user');
+                    window.location.href = "/";
+                  }}
+                > Logout </Button>
+              </div>
 
+              <div>
+
+                {user?.picture && !imgError ? (
+                  <img
+                    src={user?.picture}
+                    className="h-10 w-11 rounded-full object-cover  hidden md:block"
+                    alt="user"
+                    onError={() => setImgError(true)}
+                  />
+
+                ) :
+                  <div >
+                    <FaUserCircle className='h-10 w-12 text-gray-500  hidden md:block' />
+                    {/* <div className='p-1 font-light text-sm'>{user.name}</div> */}
+                  </div>
+
+                }
+              </div>
             </div>
+
           </div>
           :
           // <ButtonSecondary/>
-            <Button className="rounded-2xl transition-all delay-100 duration-150 place-self-end-safe hover:bg-gray-700 cursor-pointer hover:scale-x-105"
-            onClick={()=>setOpenDialog(true)}
-            >Sign In</Button>
-          }
-           </div>
+          <Button className="rounded-2xl transition-all delay-100 duration-150 place-self-end-safe hover:bg-gray-700 cursor-pointer hover:scale-x-105"
+            onClick={() => setOpenDialog(true)}
+          >Sign In</Button>
+        }
+      </div>
 
-          <Dialog open={openDialog}>
-          
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogDescription>
-          
-                        <img src="/logo.svg" className='h-22 w-55 ml-22' />
-                        <h2 className='font-bold text-lg mt-2'>Sign In with Google</h2>
-                        <p>Sign In with Google authentication securely!</p>
-                        <Button
-                          className='w-full mt-5 gap-3 items-center'
-                          onClick={login}
-                        >
-                          <FaGoogle className='h-7 w-7' />Sign In</Button>
-                      </DialogDescription>
-                    </DialogHeader>
-                  </DialogContent>
-          
-          </Dialog>
+      <Dialog open={openDialog}>
 
-     
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+
+              <img src="/logo.svg" className='h-22 w-55 ml-22' />
+              <h2 className='font-bold text-lg mt-2'>Sign In with Google</h2>
+              <p>Sign In with Google authentication securely!</p>
+              <Button
+                className='w-full mt-5 gap-3 items-center'
+                onClick={login}
+              >
+                <FaGoogle className='h-7 w-7' />Sign In</Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+
+      </Dialog>
+
+
     </div>
   )
 }
